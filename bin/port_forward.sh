446d7b06eb8374e4d315e03174f8e73b008d57c1
Gino Prasad <ginoprasad@gmail.com>
2026-01-08 13:50:18 -0800

    Streamlined Pod Startup

--- bin/port_forward.sh
+++ bin/port_forward.sh
@@ -7,7 +7,7 @@ port=$2
 ERROR=$(kubectl port-forward $pod_name $port:$port  2>&1 | grep 'lost connection' )
 while [[ $ERROR != '' ]]
 do
-  ERROR=$(kubectl port-forward $pod_name $port:$port 2>&1 | grep 'lost connection' ); 
-  sleep 5; 
+  ERROR=$(kubectl port-forward $pod_name $port:$port 2>&1 | grep 'lost connection' )
+  sleep 5
 done
 
